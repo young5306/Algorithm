200c3345e5df307cbec275bb001a6cd3be8a6afb
young5306 <140944183+young5306@users.noreply.github.com>
2024-08-23 19:55:21 +0900

    [level 0] Title: 배열 만들기 2, Time: 14.60 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,15 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        int[] answer = IntStream.rangeClosed(l, r).filter(i->{
+			while(i>0) {
+				if(i%5!=0) return false;
+				i/=10;
+			}
+			return true;
+		}).toArray();
+		
+		return answer.length != 0 ? answer : new int[]{-1};
+    }
+}
