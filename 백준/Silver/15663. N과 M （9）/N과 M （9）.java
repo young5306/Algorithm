e4c484711b29cf1bcd9457714f2eb93bf0f689c6
young5306 <140944183+young5306@users.noreply.github.com>
2024-09-04 16:34:36 +0900

    [Silver II] Title: N과 M (9), Time: 204 ms, Memory: 23948 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,92 +1,59 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
 
+	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws Exception {
-		// N개 중 M개 (중복X, 순서O) -> 중복X 순열
-		// 입력되는 수는 중복 -> 다른 숫자로 생각
-		// 사전순 -> nums 정렬
 		
+		// 중복X, 순서O -> 순열
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[] nums = new int[N];
+		Map<Integer, Integer> map = new TreeMap<>(); // <값, 개수>
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(nums);
-		int[] sel = new int[M];
-		boolean[] visited = new boolean[N];
-		List<int[]> result = new ArrayList<>();
-		combination(N, M, nums, visited, sel, result, 0);
-		
-		for (int[] arr : result) {
-			for (int i : arr) {
-				sb.append(i).append(" ");
+			
+			int key = Integer.parseInt(st.nextToken());
+			if(map.containsKey(key)) {
+				map.put(key, map.get(key) + 1);
+			} else {
+				map.put(key, 1);
 			}
-			sb.append("\n");
 		}
+		
+		int[] sel = new int[M];
+		
+		perm(map, sel, M, 0);
 		
 		System.out.println(sb);
-		
-
 	}
 	
-	public static void combination(int N, int M, int[] nums, boolean[] visited, int[] sel, List<int[]> result, int sidx) {
+	public static void perm(Map<Integer, Integer> map, int[] sel, int M, int sidx) {
 		// 기저 조건
 		if(sidx == M) {
-			// 똑같은 순열 있는지 확인해서 없으면 append 하는거
-			int[] test = Arrays.copyOf(sel, M); // 새로운 배열을 만들어서 넣어야 함. 
-			// 주소값 같은 배열로 계속 넣으면 결국 같은 배열만 여러개 든 리스트됨.
-			for (int[] arr : result) {
-				if(Arrays.equals(arr, test)) return; // arr.equals(test) 하면 안됨
+			for (int i = 0; i < M; i++) {
+				sb.append(sel[i]).append(" ");
 			}
-			result.add(test);
+			sb.append("\n");
 			return;
 		}
-		
 		// 재귀 부분
-		for (int i = 0; i < N; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				sel[sidx] = nums[i];
-				combination(N, M, nums, visited, sel, result, sidx+1);
-				visited[i] = false;
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			if(entry.getValue()>0) {
+				map.put(entry.getKey(), entry.getValue()-1);
+				sel[sidx] = entry.getKey();
+				perm(map, sel, M, sidx+1);
+				map.put(entry.getKey(), entry.getValue()+1);
 			}
 		}
 	}
 
 }
-
-//String str1 = new String("one");
-//String str2 = new String("one");
-//String str3 = "two";
-//String str4 = "two";
-//System.out.println(str1==str2); // false - 주소가 다름
-//System.out.println(str3==str4); // true - 문자열 풀에 있는 문자열 재사용해서 주소 같음
-//System.out.println(str1.equals(str2)); // true - 값 비교
-//
-//int[] arr1 = {1,2,3};
-//int[] arr2 = {1,2,3};
-//System.out.println(arr1==arr2); // false - 주소 다름
-//System.out.println(arr1.equals(arr2)); // false - Object.equals는 주소비교
-//System.out.println(Arrays.equals(arr1, arr2)); //  true - 배열 값 비교
-
-// (정리)
-// String 
-// == : 주소 비교
-// equals : 값비교
-
-// Object
-// Object.equals : == 비교 (주소 비교)
-// Arrays.equals(arr1, arr2) : 배열 값 비교
