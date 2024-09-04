ed38f268b5725eec337ea490dbadb8510afa44e5
young5306 <140944183+young5306@users.noreply.github.com>
2024-09-04 15:42:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 173 ms, Memory: 23,692 KB -BaekjoonHub

---
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+public class Solution {
+	
+	static int[] cnt;
+	static int[] nums;
+	static int N;
+	
+	static int max;
+	static int min;
+
+	public static void main(String[] args) {
+		// 중복된 입력값 있음 -> visit배열을 cnt배열로 대체
+		// 순서O - 순열
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int t = 1; t <= T; t++) {
+			N = sc.nextInt();
+			
+			nums = new int[N];
+			cnt = new int[4];
+			
+			for (int i = 0; i < 4; i++) {
+				cnt[i] = sc.nextInt();
+			}
+			
+			for (int i = 0; i < N; i++) {
+				nums[i] = sc.nextInt();
+			}
+			
+			
+			max = -100000001;
+			min = 100000001;
+			// 연산자 배치
+			perm(0, nums[0]); // sidx
+			
+			
+			
+			System.out.println("#"+t+" "+(max-min));
+		}
+	}
+	
+	static void perm(int sidx, int res){ // 중간 연산 값 들고 다니기
+		// 기저 조건
+		if(sidx==N-1) {
+			min = Math.min(res, min);
+			max = Math.max(res, max);
+			return;
+		}
+		
+		// 재귀 부분
+		for (int op = 0; op < 4; op++) {
+			if(cnt[op]>0) {
+				cnt[op]--;
+				if(op==0) {
+					perm(sidx+1, res + nums[sidx+1]);
+				} else if (op==1) {
+					perm(sidx+1, res - nums[sidx+1]);
+				} else if (op==2) {
+					perm(sidx+1, res * nums[sidx+1]);
+				} else if (op==3) {
+					perm(sidx+1, res / nums[sidx+1]);
+				} 
+				cnt[op]++;
+			}
+		}
+	}
+
+}
