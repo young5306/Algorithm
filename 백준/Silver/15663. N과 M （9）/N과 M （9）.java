e9e04dd12dc040f89e1f85d93958cd23447ebd5d
young5306 <140944183+young5306@users.noreply.github.com>
2024-09-05 08:57:04 +0900

    [Silver II] Title: N과 M (9), Time: 172 ms, Memory: 20928 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 public class Main {
 
@@ -11,32 +10,30 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		
 		// 중복X, 순서O -> 순열
+		// 입력값이 중복
+		// 정렬한 후 이전에 선택했던 값이랑 똑같으면 넘어가기
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
-		Map<Integer, Integer> map = new TreeMap<>(); // <값, 개수>
+	
 		st = new StringTokenizer(br.readLine());
+		int[] nums = new int[N];
 		for (int i = 0; i < N; i++) {
-			
-			int key = Integer.parseInt(st.nextToken());
-			if(map.containsKey(key)) {
-				map.put(key, map.get(key) + 1);
-			} else {
-				map.put(key, 1);
-			}
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
+		Arrays.sort(nums);
 		
 		int[] sel = new int[M];
+		boolean[] visited = new boolean[N];
 		
-		perm(map, sel, M, 0);
+		perm(nums, sel, visited, N, M, 0); 
 		
 		System.out.println(sb);
 	}
 	
-	public static void perm(Map<Integer, Integer> map, int[] sel, int M, int sidx) {
+	public static void perm(int[] nums, int[] sel, boolean[] visited, int N, int M, int sidx) {
 		// 기저 조건
 		if(sidx == M) {
 			for (int i = 0; i < M; i++) {
@@ -45,15 +42,21 @@ public class Main {
 			sb.append("\n");
 			return;
 		}
-		// 재귀 부분
-		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-			if(entry.getValue()>0) {
-				map.put(entry.getKey(), entry.getValue()-1);
-				sel[sidx] = entry.getKey();
-				perm(map, sel, M, sidx+1);
-				map.put(entry.getKey(), entry.getValue()+1);
+		
+		// 재귀 부분 - 같은 자리(sidx)에 같은 수가 안나오게
+		// 연이어 중복 수 나올 때 첫 수만 선택되고 중복으로 들어온 다음 수는 선택 안되게
+		int before = 0; // N범위에 0없어서 첫수는 항상 선택됨 
+		for (int i = 0; i < N; i++) {
+			if(!visited[i] && before!=nums[i]) {
+				visited[i] = true;
+				sel[sidx] = nums[i];
+				before = sel[sidx];
+				perm(nums, sel, visited, N, M, sidx+1); // 선택O의 경우 36번줄로 가서 before=0됨.
+				visited[i] = false;
+				// 선택X의 경우, 49번줄로 가서 before=sel[sidx]상태. 
+				// sidx자리에 nums[i+1]번 들어오는데, 
+				// 이전과 같은 수라면 들어오지 못하고, i+2로 넘어가게
 			}
 		}
 	}
-
 }
