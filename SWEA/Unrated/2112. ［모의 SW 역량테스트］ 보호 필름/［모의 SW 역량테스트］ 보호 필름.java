17eb6d3f46ed4716297a7af2b74e3ea79248b13d
young5306 <140944183+young5306@users.noreply.github.com>
2024-09-08 23:08:23 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 446 ms, Memory: 36,884 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -1,3 +1,5 @@
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
@@ -5,6 +7,7 @@ public class Solution {
 	static int D, W, K; // D:행 3~13, W:열 1~20, K:통과기준1~D 
 	static int[][] film; // 보호필름 저장
 	static int ans; // 정답(최소 투약 횟수)
+	static int[] A, B;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -14,6 +17,11 @@ public class Solution {
 			W = sc.nextInt();
 			K = sc.nextInt();
 			film = new int[D][W];
+			A = new int[W]; // 이미 0으로 초기화 되어 있음
+			B = new int[W]; // 전부 1로 만들어 주자
+			
+			Arrays.fill(B, 1);
+			
 			for (int i = 0; i < D; i++) {
 				for (int j = 0; j < W; j++) {
 					film[i][j] = sc.nextInt();
@@ -31,6 +39,7 @@ public class Solution {
 	// idx : 현재 약을 주입하려고 하는 행
 	// cnt : 약품을 주입한 횟수
 	private static void makefilm(int idx, int cnt) {
+		if(ans < cnt) return;
 		if(idx==D) {
 			// 이제 확인을 하겠다.
 			if(isOk()) {
@@ -41,35 +50,17 @@ public class Solution {
 		
 		// 1. 주입 X
 		makefilm(idx+1, cnt);
-		
-		int[] tmp = new int[W];
-		for (int i = 0; i < W; i++) {
-			tmp[i] = film[idx][i]; // 깊은 복사
-		}
+		int[] tmp = film[idx];
 		
 		// 2. 주입 A
-//		for (int i = 0; i < W; i++) {
-//			film[idx][i] = 0;
-//		}
-		yak(idx, 0);
+		film[idx] = A;
 		makefilm(idx+1, cnt+1);
 		// 3. 주입 B
-//		for (int i = 0; i < W; i++) {
-//			film[idx][i] = 1;
-//		}
-		yak(idx, 1);
+		film[idx] = B;
 		makefilm(idx+1, cnt+1);
 		
 		// 원상복구!
-		for (int i = 0; i < W; i++) {
-			film[idx][i] = tmp[i];
-		}
-	}
-	
-	private static void yak(int idx, int drug) {
-		for (int i = 0; i < W; i++) {
-			film[idx][i] = drug;
-		}
+		film[idx] = tmp;
 	}
 	
 	// 모든 열이 연속된 특성 K개 이상인가?
