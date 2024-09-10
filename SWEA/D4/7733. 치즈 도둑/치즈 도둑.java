792e00f6b83abec362d6a1d43fd8b226f2e24f72
young5306 <140944183+young5306@users.noreply.github.com>
2024-09-10 15:08:28 +0900

    [D4] Title: 치즈 도둑, Time: 394 ms, Memory: 87,220 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -1,6 +1,8 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-public class Solution {
+public class Solution { // BFS
 	
 	static int N;
 	static int[][] board;
@@ -23,24 +25,26 @@ public class Solution {
 				}
 			} // 입력 완료
 			
+			// bfs
 			// 1. 1~100일 동안 각 날마다 매번 모든 요소 돌기
-			// 2. 시작점 : 매번 (0,0)부터 방문안한, day보다 큰 곳 방문해서 dfs 
-			// 3. 다음 dfs 조건 : (nr,nc)가 board 범위 내, visited 안한 곳, day보다 큰 곳
-			// 4. visited 조건 : 각 날짜마다 방문할 수 있는 곳(day 초과) 중에서만 방문 체크 (날마다 초기화-덩어리 다시 세야해서)
-			// cnt(덩어리세기) 언제 하나 : 시작점 dfs순회 끝날때 cnt+1해서 덩어리 세기, visited는 초기화
-			// 다음 day에서 반복 : 다음 위치는 day보다 큰 곳부터 시작
-			
-			int max = 1; // 0일에는 한덩어리
+			// 2. 시작점 : 매번 (0,0)부터 방문안한, day보다 큰 곳 방문
+			// 3. bfs 시작 : queue에 시작점 넣고, 방문체크
+			// while : 시작점 빼면서 인접점(nr,nc) 넣음 
+			// 인접점(nr,nc) 조건 : board 범위 내, day보다 크고, 방문 안한 곳
+			// 4. visited 조건 : 각 날짜마다 방문할 수 있는 곳(day 초과) 중에서만 방문 체크 
+			// visited는 날마다 초기화-덩어리 다시 세야해서
+			// cnt(덩어리세기) 언제 하나 : queue 다 빠질 때마다 cnt+1 (cnt도 날마다 초기화)
+		
+			int max = 1;
 			for (int day = 1; day <= 100; day++) { // 1.
 				
-				visited = new boolean[N][N]; // 4.
+				visited = new boolean[N][N];
 				int cnt = 0;
 				
 				for (int r = 0; r < N; r++) {
 					for (int c = 0; c < N; c++) {
-						if(board[r][c]>day && !visited[r][c]) { // 2.
-							visited[r][c] = true;
-							dfs(r,c, day);
+						if(!visited[r][c] && board[r][c]>day) {
+							bfs(r, c, day);
 							cnt++;
 						}
 					}
@@ -55,19 +59,26 @@ public class Solution {
 
 	} // main
 	
-	static void dfs(int r, int c, int day) { 
-		for (int d = 0; d < 4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
+	static void bfs(int r, int c, int day) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[]{r,c});
+		visited[r][c] = true;
+		
+		while(!q.isEmpty()) {
+			int[] place = q.poll();
 			
-			if(nr>=0 && nr<N && nc>=0 && nc<N && !visited[nr][nc] && board[nr][nc]>day) { // 3.
-				visited[nr][nc] = true;
-				dfs(nr, nc, day);
+			for (int d = 0; d < 4; d++) {
+				int nr = place[0] + dr[d];
+				int nc = place[1] + dc[d];
+				
+				if(nr>=0 && nr<N && nc>=0 && nc<N && !visited[nr][nc] && board[nr][nc]>day) {
+					visited[nr][nc] = true;
+					q.add(new int[] {nr, nc});
+				}
 			}
 		}
+		
 	}
-	
-	// 나는 day보다 작은거 일단 다 visited 표시 후 board 또 돌면서 덩어리 셈
-	// 강사님 풀이 : board[nr][nc]>day 조건으로 day보다 작은거 그냥 무시 => 연산 훨씬 줆
+
 
 }
