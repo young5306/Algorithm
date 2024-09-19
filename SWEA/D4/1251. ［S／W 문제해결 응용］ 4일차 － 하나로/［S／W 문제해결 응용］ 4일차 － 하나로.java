b9bb97f0e8259a9c37f01f6f6e67a2ef7b90a2bd
young5306 <140944183+young5306@users.noreply.github.com>
2024-09-20 00:25:31 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 530 ms, Memory: 112,444 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -8,12 +6,13 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	
 	static class Edge implements Comparable<Edge> {
+		int A;
 		int B;
 		double W;
-		public Edge(int b, double w) {
+		public Edge(int a, int b, double w) {
 			super();
+			A = a;
 			B = b;
 			W = w;
 		}
@@ -24,67 +23,59 @@ public class Solution {
 	}
 
 	public static void main(String[] args) throws Exception {
-		
-		// 최소 비용으로 모든 점 연결
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
 		int T = Integer.parseInt(br.readLine());
-		
 		for (int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(br.readLine());
-			// 1. 입력 받기 : 각 좌표에 0~N-1번 부여, Edge(B,W) 입력
-			int[][] land= new int[N][2];
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				land[i][0] = Integer.parseInt(st.nextToken()); // x좌표
-			}
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				land[i][1] = Integer.parseInt(st.nextToken()); // y좌표
-			}
+			int[][] place = new int[N][2];
+			for (int i = 0; i < 2; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					place[j][i] = Integer.parseInt(st.nextToken());
+				}
+			} 
 			double E = Double.parseDouble(br.readLine());
 			// 입력 완료
+			
 			List<Edge>[] adj = new ArrayList[N];
 			for (int i = 0; i < N; i++) {
 				adj[i] = new ArrayList<>();
 			}
 			for (int i = 0; i < N-1; i++) {
 				for (int j = i+1; j < N; j++) {
-					int x1 = land[i][0];
-					int y1 = land[i][1];
-					int x2 = land[j][0];
-					int y2 = land[j][1];
-					
-					double w = Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2));
-					adj[i].add(new Edge(j,w));
-					adj[j].add(new Edge(i,w)); // 무향
+					// 거리
+					double distance = Math.sqrt(Math.pow(place[i][0]-place[j][0], 2) + Math.pow(place[i][1]-place[j][1], 2));
+					adj[i].add(new Edge(i, j, distance));
+					adj[j].add(new Edge(j, i, distance));
 				}
-			} // 간선 저장 완료
+			} // 인접리스트 저장 완료
 			
-			// 0번 점에서 시작
+			// 프림
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+			pq.addAll(adj[0]); // 0번부터 시작
 			boolean[] visited = new boolean[N];
 			visited[0] = true;
-			// pq에 연결간선 담기
-			PriorityQueue<Edge> pq = new PriorityQueue<>();
-			pq.addAll(adj[0]);
 			
-			double ans = 0;
 			int pick = 1;
-			while(pick!=N) {
+			double ans = 0;
+			
+			while(pick != N) {
 				Edge e = pq.poll();
+				
 				if(visited[e.B]) continue;
 				
-				ans += Math.pow(e.W,2);
 				pick++;
-				visited[e.B] = true;
+				ans += Math.pow(e.W, 2) * E;
 				
+				visited[e.B] = true;
 				pq.addAll(adj[e.B]);
 			}
 			
-			System.out.println("#"+tc+" "+Math.round(ans*E));
+			System.out.printf("#%d %.0f\n", tc, ans);
+			
 		}
+
 	}
 
 }
