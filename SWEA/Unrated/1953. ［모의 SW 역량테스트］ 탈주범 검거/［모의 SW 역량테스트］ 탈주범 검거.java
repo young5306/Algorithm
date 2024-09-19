90e7f8de9c7414b461f970cd5688e5cfc3491245
young5306 <140944183+young5306@users.noreply.github.com>
2024-09-19 23:57:12 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 177 ms, Memory: 25,892 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -1,7 +1,4 @@
-
-
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -9,101 +6,102 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	
-	// 상우하좌
-	static int[] dr = {-1,0,1,0};
-	static int[] dc = {0,1,0,-1};
-	static int[][] map;
-	
-	static int[][] dCheck = {{}, {0,1,2,3}, {0,2}, {1,3}, {0,1}, {1,2}, {2,3}, {0,3}};
-	// 뺀 파이프 확인
-	// 1 - 0123 / 2 - 02 / 3- 13 / 4 - 01 / 5 - 12 / 6 - 23 / 7 - 03
-	
-	
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+
 	public static void main(String[] args) throws Exception {
 		
-		// 수업 코드 보자.
-		// 따라쳐보고 혼자쳐보고 문제 풀자
-		// bfs > dijk
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
 		
 		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int N = Integer.parseInt(st.nextToken()); // 세로
 			int M = Integer.parseInt(st.nextToken()); // 가로
-			int R = Integer.parseInt(st.nextToken()); // 맨홀뚜껑 위치r
-			int C = Integer.parseInt(st.nextToken()); // 위치c
-			int L = Integer.parseInt(st.nextToken()); // 탈출 후 소요 시간
+			int R = Integer.parseInt(st.nextToken()); // 첫위치R
+			int C = Integer.parseInt(st.nextToken()); // 첫위치 C
+			int L = Integer.parseInt(st.nextToken()); // 소요시간
 			
-			map = new int[N][M];
+			int[][] tunnel = new int[N][M];
 			for (int r = 0; r < N; r++) {
 				st = new StringTokenizer(br.readLine());
 				for (int c = 0; c < M; c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
+					tunnel[r][c] = Integer.parseInt(st.nextToken());
 				}
-			} // 입력 완료
+			}
 			
-			// bfs - R,C 부터 시작
-			Queue<int[]> queue = new LinkedList<>();
+			// bfs
+			Queue<int[]> q = new LinkedList<>();
 			boolean[][] visited = new boolean[N][M];
 			
+			q.add(new int[]{R, C});
 			visited[R][C] = true;
-			queue.add(new int[] {R, C, 1}); // r, c, level
+//			System.out.println("RC"+R+","+C);
+			int time = 1;
 			int cnt = 1;
-			int level = 0;
 			
-			while(!queue.isEmpty()) {
+			while(!q.isEmpty()) {
 				
+				if(time==L) break;
 				
-				int[] place = queue.poll();
-				int number = map[place[0]][place[1]];
-				level = place[2]; 
-				
-				// 뺀 파이프 확인
-				// 1 - 0123 / 2 - 02 / 3- 13 / 4 - 01 / 5 - 12 / 6 - 23 / 7 - 03
-				// 연결될 수 있는 파이프
-				// 상 - 1256 / 우 - 1367 / 하 - 1247 / 좌 - 1345
-				for (int d = 0; d < dCheck[number].length; d++) {
-					int nr = place[0] + dr[dCheck[number][d]];
-					int nc = place[1] + dc[dCheck[number][d]];
+				// bfs에서 level 구하기!
+				int qSize = q.size();
+				for (int qs = 0; qs < qSize; qs++) {
 					
-					if(nr>=0 && nr<N && nc>=0 && nc<M && !visited[nr][nc] && isAvailable(dCheck[number][d], nr, nc) && level<L) {
-						visited[nr][nc] = true;
-						cnt++;
-						queue.add(new int[] {nr, nc, level+1}); 
+					int[] place = q.poll();
+					int r = place[0];
+					int c = place[1];
+//					System.out.println("poll "+r+","+c);
+					
+					for (int i = 0; i < 4; i++) {
+						int nr = r + dr[i];
+						int nc = c + dc[i];
+						// 방문한 적 없고, 이동 가능한 곳
+						if(nr>=0 && nr<N && nc>=0 && nc<M && !visited[nr][nc] && isAvailable(tunnel[r][c], i, tunnel[nr][nc])) {
+//							System.out.println(nr+","+nc);
+							q.add(new int[] {nr,nc});
+							visited[nr][nc] = true;
+							cnt++;
+						}
 					}
-				}
+				} // for qSize
+				time++;
 				
-			}
+			} // while
 			
 			System.out.println("#"+tc+" "+cnt);
-			
-		} // tc
-		
-	} // main
+		}
+	}
 	
-	static boolean isAvailable(int prev, int r, int c) {
-		if(prev==0) { // 상
-			if(map[r][c] == 1 || map[r][c] == 2 || map[r][c] == 5 || map[r][c] == 6) {
-				return true;
-			}
-		} else if(prev==1) { // 우
-			if(map[r][c] == 1 || map[r][c] == 3 || map[r][c] == 6 || map[r][c] == 7) {
-				return true;
-			}
-		} else if(prev==2) { // 하
-			if(map[r][c] == 1 || map[r][c] == 2 || map[r][c] == 4 || map[r][c] == 7) {
-				return true;
-			}
-		} else if(prev==3) { // 좌
-			if(map[r][c] == 1 || map[r][c] == 3 || map[r][c] == 4 || map[r][c] == 5) {
-				return true;
+	static boolean isAvailable(int curr, int dir, int next) {
+		// 1. 0이면 바로 false
+		if(next==0) return false;
+		
+		// 2. 이동가능한지
+		// curr
+		// 상으로 이동가능 : 1247
+		// 하로 이동가능  : 1256
+		// 우로 이동가능  : 1345
+		// 좌으로 이동가능 : 1367 
+		int[][] currArr = {{1,2,4,7},{1,3,4,5},{1,2,5,6},{1,3,6,7}};
+		// next
+		// 상과 연결 : 1256
+		// 하와 연결 : 1247
+		// 우와 연결 : 1367
+		// 좌과 연결 : 1345
+		int[][] nextArr = {{1,2,5,6},{1,3,6,7},{1,2,4,7},{1,3,4,5}};
+		
+		for (int i = 0; i < 4; i++) {
+			if(curr == currArr[dir][i]) {
+				for (int j = 0; j < 4; j++) {
+					if(next == nextArr[dir][j]) {
+						return true;
+					}
+				}
 			}
 		}
 		return false;
 	}
+
 }
