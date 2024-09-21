496e578598adab8e3bedc82efe74c8ab0cd733b7
young5306 <140944183+young5306@users.noreply.github.com>
2024-09-21 16:30:35 +0900

    [D3] Title: 햄버거 다이어트, Time: 205 ms, Memory: 22,388 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution { // 1차 배열
+public class Solution { // 1차배열
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
