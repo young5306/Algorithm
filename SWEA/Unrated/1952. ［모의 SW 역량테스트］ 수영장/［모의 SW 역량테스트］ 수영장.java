5c23f728f5ed5dfd0d0d631acea26a02116a0096
young5306 <140944183+young5306@users.noreply.github.com>
2024-09-25 17:50:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 163 ms, Memory: 25,664 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,61 +1,58 @@
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-	
-	static int[] price; // 4
-	static int[] plan; // 12
-	static int min;
-	
-	public static void main(String[] args) throws FileNotFoundException {
 
-		Scanner sc = new Scanner(System.in);
-//		Scanner sc = new Scanner(new FileInputStream("input.txt"));
-		int T = sc.nextInt();
-		
-		for (int t = 1; t <= T; t++) {
-			
-			price = new int[4]; // 1일, 1달, 3달, 1년 순
-			for (int i = 0; i < 4; i++) {
-				price[i] = sc.nextInt();
-			}
-			
-			plan = new int[12]; // 매달 이용 계획
-			for (int i = 0; i < 12; i++) {
-				plan[i]= sc.nextInt();
-			}
-			
-			min = 2000000;  // 3000원*31*12 = 372*3000 = 1116000
-			recur(0, 0);
-			
-			System.out.println("#"+t+" "+min);
-		}
-		
-	}
-	
-	public static void recur(int midx, int cost) { // 월인덱스, 중간값 들고 다니기
-		// 기저 조건
-		if(midx >= 12) {
-			min = Math.min(min, cost);
-			return;
-		}
-		// 재귀 조건 (조합 느낌)
-		if(plan[midx]==0) {
-			recur(midx+1, cost);
-		} else {
-			for (int i = 0; i < 4; i++) {
-				if(plan[midx]!=0) {
-					if(i==0) { // 1일권
-						recur(midx+1, cost+plan[midx]*price[i]);
-					} else if (i==1) { // 1달권
-						recur(midx+1, cost+price[i]);
-					} else if (i==2) { // 3달권
-						recur(midx+3, cost+price[i]);
-					} else if (i==3) { // 1년권
-						recur(midx+12, cost+price[i]);
-					}
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int day = Integer.parseInt(st.nextToken());
+			int mon = Integer.parseInt(st.nextToken());
+			int threeMon = Integer.parseInt(st.nextToken());
+			int year = Integer.parseInt(st.nextToken());
+
+			int[] plan = new int[13]; // 0~12
+			st = new StringTokenizer(br.readLine());
+			for (int i = 1; i <= 12; i++) {
+				plan[i] = Integer.parseInt(st.nextToken());
+			} // 입력 완료
+
+			int[] dp = new int[13];
+
+			for (int j = 1; j <= 12; j++) { // 임시 월
+				if(plan[j]!=0) { 
+					
+					// 1일권 사용하는 경우
+					int minCnt = Math.min(987654321, dp[j-1] + plan[j]*day);
+					// 1일, 1달권 사용하는 경우 
+					minCnt = Math.min(minCnt, dp[j-1]+mon);
+//					int minCnt = dp[j-1] + Math.min(plan[j]*day, mon); // 1일, 1달권 합쳐서 코드 작성 가능
+					
+					// 1일, 1달, 3달권 사용하는 경우
+					if(j>=3) minCnt = Math.min(minCnt, dp[j-3]+threeMon);
+					else minCnt = Math.min(minCnt, threeMon);
+					
+					// 1일, 1달, 3달권, 1년권 사용하는 경우
+					if(j>=12) minCnt = Math.min(minCnt, dp[j-12]+year);
+					else minCnt = Math.min(minCnt, year);
+					
+					dp[j] = minCnt;
+				} else {
+					dp[j] = dp[j-1];
 				}
+				
 			}
+//			System.out.println(Arrays.toString(dp));
+			
+			System.out.println("#"+tc+" "+dp[12]);
 		}
+
 	}
+
 }
