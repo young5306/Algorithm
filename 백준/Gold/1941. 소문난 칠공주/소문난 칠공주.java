56a27c53e5fe03775bee8b52dea060933a39ef92
young5306 <140944183+young5306@users.noreply.github.com>
2024-10-30 12:48:14 +0900

    [Gold III] Title: 소문난 칠공주, Time: 316 ms, Memory: 77472 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,14 +1,18 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
+// selected, visited (체크하는 배열) -> 1차배열로 풀기
+// => 장점 : 69번줄에서 for문 7번만 돌아도 됨
 public class Main {
 	
 	static char[][] stu;
-	static boolean[][] selected;
+	static int[] selected = new int[7]; // 1차배열 (-> 2차배열로 바꾸기 [i/5][i%5])
+	
 	static int[] dr = {-1,0,1,0};
 	static int[] dc = {0,1,0,-1};
 	static int cnt;
@@ -17,81 +21,70 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		stu = new char[5][5];
 		for (int i = 0; i < 5; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < 5; j++) {
-				stu[i][j] = str.charAt(j);
-			}
+			stu[i] = br.readLine().toCharArray(); // 한줄로 작성 가능
 		} // 입력 완료
 		
 		// 1. 7명 고르기 (조합)
-		selected = new boolean[5][5];
 		cnt = 0;
-		comb(0,0,0,0); // r, c, sidx, S수
+		bt(0,0,0); // depth(sidx), numY, start(idx)
 		
 		System.out.println(cnt);
 	} // main
 	
-	static void comb(int r, int c, int sidx, int scnt) {
+	static void bt(int depth, int numY, int start) {
 		// 기저 조건
-		if(sidx == 7) {
-			// S가 4명 이상
-			if(scnt<4) {
-				return;
-			}
-			// 인접한지 확인
-			if(available()) {
-				cnt++;
-			}
+		if(numY>=4) return;
+		
+		if(depth==7) {
+			bfs();
 			return;
 		}
 		
-		if(r==5) return;
-		
 		// 재귀 부분
-		 // 선택O
-		selected[r][c] = true;
-		if(stu[r][c]=='S') scnt++;
-		if(c+1==5) comb(r+1, 0, sidx+1, scnt);
-		else comb(r, c+1, sidx+1, scnt);
-		if(stu[r][c]=='S') scnt--;
-		 // 선택X
-		selected[r][c] = false;
-		if(c+1==5) comb(r+1, 0, sidx, scnt);
-		else comb(r, c+1, sidx, scnt);
+		for (int i = start; i < 25; i++) {
+			// 선택O
+			selected[depth] = i;
+			if(stu[i/5][i%5]=='Y') bt(depth+1, numY+1, i+1);
+			else bt(depth+1, numY, i+1);
+		}
 	}
 	
-	static boolean available() {
+	static void bfs() {
 		Queue<int[]> q = new LinkedList<>();
-		boolean[][] visited = new boolean[5][5];
-		out : for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				if(selected[i][j]) {
-					q.add(new int[]{i,j});
-					visited[i][j] = true;
-					break out;
-				}
-			}
-		}
-		
+		boolean[] visited = new boolean[7]; 
+		q.add(new int[] {selected[0]/5, selected[0]%5}); 
+		visited[0] = true;
 		int pick = 1;
+		
 		while(!q.isEmpty()) {
-			int[] v = q.poll();
-			int r = v[0];
-			int c = v[1];
+			int[] cur = q.poll();
+			int r = cur[0];
+			int c = cur[1];
 			for (int d = 0; d < 4; d++) {
 				int nr = r + dr[d];
 				int nc = c + dc[d];
-				if(nr>=0 && nr<5 && nc>=0 && nc<5 && !visited[nr][nc] && selected[nr][nc]) {
-					visited[nr][nc] = true;
-					pick++;
-					q.add(new int[] {nr, nc});
+				int ni = nr*5 + nc;
+				
+				if(!isValid(nr, nc)) continue;
+				
+				for (int i = 0; i < 7; i++) { // selected 모두 돌면서 인접한 공주있는지 확인 
+					if(!visited[i] && selected[i] == ni) {
+						visited[i] = true;
+						pick++;
+						q.add(new int[] {nr, nc});
+					}
 				}
+				
 			}
 		}
 		
-		if(pick==7) return true;
-		else return false;
+		if(pick==7) cnt++;
 		
+	}
+	
+	static boolean isValid(int r, int c) {
+		if(r>=0 && r<5 && c>=0 && c<5) return true;
+		else return false;
 	}
 
 }
