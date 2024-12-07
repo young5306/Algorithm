2980a6e17f6fa1e862d522652bfc367d084d5711
young5306 <140944183+young5306@users.noreply.github.com>
2024-12-07 22:55:01 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 484 ms, Memory: 53728 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -2,16 +2,13 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	//comparator 연습
+	//comparable 연습
 	
-	static class Node {
+	static class Node implements Comparable<Node>{
 		int x;
 		int y;
 		
@@ -20,6 +17,11 @@ public class Main {
 			this.x = x;
 			this.y = y;
 		}
+		@Override
+		public int compareTo(Node o) {
+			if(this.y != o.y) return this.y - o.y;
+			else return this.x - o.x;
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -27,25 +29,19 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st;
 		
-		List<Node> list = new LinkedList<>();
+		PriorityQueue<Node> pq = new PriorityQueue<>();
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			
-			list.add(new Node(x, y));
+			pq.add(new Node(x, y));
 		}
 		
-		Collections.sort(list, new Comparator<Node>() {
-			@Override
-			public int compare(Node o1, Node o2) {
-				if(o1.y != o2.y) return o1.y -o2.y; // 오름차순
-				else return o1.x - o2.x;
-			}
-		});
 		StringBuilder sb= new StringBuilder();
-		for (Node node : list) {
+		while(!pq.isEmpty()) {
+			Node node = pq.poll();
 			sb.append(node.x).append(" "). append(node.y).append("\n");
 		}
 		
