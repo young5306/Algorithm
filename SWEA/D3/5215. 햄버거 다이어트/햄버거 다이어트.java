490172f2c43a738a46ec68e8f5e92a10cd20d95d
young5306 <140944183+young5306@users.noreply.github.com>
2025-01-27 14:33:02 +0900

    [D3] Title: 햄버거 다이어트, Time: 205 ms, Memory: 22,388 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,41 +1,65 @@
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution { // 1차배열
+public class Solution {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()); // 재료의 수
-			int L = Integer.parseInt(st.nextToken()); //  제한 칼로리
-			
-			// N개 재료의 맛점수, 칼로리 저장 배열
-			int[] score = new int[N];
-			int[] calory = new int[N];
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				score[i] = Integer.parseInt(st.nextToken());
-				calory[i] = Integer.parseInt(st.nextToken());
-			}
+    // 주어진 제한 칼로리 이하의 조합 중 가장 맛점수가 높은 햄버거의 점수
+    // 부분집합
 
-			int[] dp = new int[L+1]; // 0~L칼로리
-			for (int i = 0; i < N; i++) {
-				// 각 재료에 대해 현재 재료의 칼로리보다 높은 부분만 갱신
-				for (int cal = L; cal >= calory[i]; cal--) { // 역순으로 해야 이전 배열에서 갱신
-					dp[cal] = Math.max(dp[cal], dp[cal-calory[i]]+score[i]);
-				}
-				
-			}
-			// 왜 다 이렇게 푸는 거지
-			System.out.println("#"+tc+" "+dp[L]);
-			
-		}
-	}
+    static boolean[] sel;
+    static int[] taste;
+    static int[] calory;
+    static int N, L, tasteSum, calorySum, tasteMax;
 
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken()); // 재료 수
+            L = Integer.parseInt(st.nextToken()); // 제한 칼로리
+
+            taste = new int[N];
+            calory = new int[N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                taste[i] = Integer.parseInt(st.nextToken());
+                calory[i] = Integer.parseInt(st.nextToken());
+            }
+
+//            System.out.println(Arrays.toString(taste));
+//            System.out.println(Arrays.toString(calory));
+
+            // 부분집합 (재귀)
+            tasteMax = 0;
+            perm(0, 0, 0); // idx, tasteSum, calorySum
+
+            System.out.println("#"+tc+" "+tasteMax);
+        } // for tc
+    } // for main
+
+    public static void perm(int idx, int tasteSum, int calorySum){
+        // 기저 조건
+        if(calorySum > L) {
+            tasteMax = Math.max(tasteMax, tasteSum-taste[idx-1]);
+            return;
+        }
+        if(idx == N) {
+            tasteMax = Math.max(tasteMax, tasteSum);
+            return;
+        }
+
+//        System.out.println(idx+", "+tasteSum+", "+calorySum);
+        
+        // 사용
+        perm(idx+1, tasteSum+taste[idx], calorySum+calory[idx]);
+        // 사용 x
+        perm(idx+1, tasteSum, calorySum);
+    }
 }
