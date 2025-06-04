953025761d42819f25b70b449da8c43aa58d2bd1
young5306 <140944183+young5306@users.noreply.github.com>
2025-06-04 14:36:27 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 78 ms, Memory: 22,144 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,62 +1,55 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Solution {
-	
-	static int[][] maze;
-	static boolean[][] visited;
-	static boolean flag;
-	static int[] dr = {-1, 0, 1, 0};
+
+	static int[][] map;
+	static int[] dr = {-1, 0, 1, 0}; // 상우하좌
 	static int[] dc = {0, 1, 0, -1};
 	
 	public static void main(String[] args) throws Exception {
-	
+				
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for (int tc = 1; tc <= 10; tc++) {
+				
+		for(int t=1; t<=10; t++) {
+			int tc = Integer.parseInt(br.readLine());
+			map = new int[16][16];
 			
-			int T = Integer.parseInt(br.readLine());
-			
-			maze = new int[16][16];
-			int startR = 0;
-			int startC = 0;
-
-			for (int r = 0; r < 16; r++) {
-				String str = br.readLine();
-				for (int c = 0; c < 16; c++) {
-					int num = str.charAt(c) - '0';
-					maze[r][c] = num;
-					if(num == 2) {
-						startR = r;
-						startC = c;
-					}
+			String str = "";
+			for(int i=0; i<16; i++) {
+				str = br.readLine();
+				for(int j=0; j<16; j++) {
+					map[i][j] = str.charAt(j) - '0';
 				}
 			}
 			
-			visited = new boolean[16][16];
-			flag = false;
-			dfs(startR, startC);
+			// dfs
+			int result = dfs(1,1);
 			
-			System.out.println("#"+tc+" "+ (flag?1:0));
-		}
-	}
-	
-	static void dfs(int r, int c) {
-		visited[r][c] = true;
-		
-		if(maze[r][c]==3) {
-			flag = true;
-			return;
+			System.out.println("#"+tc+" "+result);
 		}
 		
-		for (int d = 0; d < 4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
-			if(!visited[nr][nc] && maze[nr][nc]!=1) {
-//				System.out.println(nr+","+nc);
-				dfs(nr, nc);
-			}
-		}
+		
 	}
 
+	static int dfs(int r, int c) {
+		// 기저 조건
+		if(map[r][c] == 3) {
+			return 1;
+		}
+		map[r][c] = 1;
+		
+		// 재귀 부분
+		for(int d=0; d<4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			
+			if(nr>=0 && nr<16 && nc>=0 && nc<16 && map[nr][nc] != 1) {
+				map[r][c] = 1; // 벽으로 바꿈
+				if(dfs(nr, nc) == 1) return 1;
+			}
+		}
+		return 0;
+	}
 }
