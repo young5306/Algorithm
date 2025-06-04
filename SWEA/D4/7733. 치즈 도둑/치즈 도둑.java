5a8ce2d66e1813b31c8be0f35b55af9fd31c1ec9
young5306 <140944183+young5306@users.noreply.github.com>
2025-06-04 16:48:58 +0900

    [D4] Title: 치즈 도둑, Time: 350 ms, Memory: 98,928 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -1,72 +1,81 @@
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Solution {
-	
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, 1, 0, -1};
+public class Solution { // 덩어리세기 bfs
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+	static int[][] board;
+	static boolean[][] visited;
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
+	static int N;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
-		
+		StringTokenizer st;
+
 		for (int tc = 1; tc <= T; tc++) {
-			
-			int N = Integer.parseInt(br.readLine());
-			int[][] cheese = new int[N][N];
-			for (int i = 0; i < N; i++) {
+			N = Integer.parseInt(br.readLine());
+
+			board = new int[N][N];
+			for (int r = 0; r < N; r++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					cheese[i][j] = Integer.parseInt(st.nextToken());
+				for (int c = 0; c < N; c++) {
+					board[r][c] = Integer.parseInt(st.nextToken());
 				}
 			}
-			
-			int max = 1;
-			for (int day = 0; day <= 100; day++) {
-				boolean[][] visited = new boolean[N][N];
+
+			int max = 1; // 0일에 덩어리 1개
+
+			// n일 - n초과 치즈만 셈
+			for (int day = 1; day <= 100; day++) {
+				visited = new boolean[N][N];
+
 				int cnt = 0;
-				
-				for (int i = 0; i < N; i++) {
-					for (int j = 0; j < N; j++) {
-						
-						if(cheese[i][j]>day && !visited[i][j]) {
+				for (int r = 0; r < N; r++) {
+					for (int c = 0; c < N; c++) {
+						// bfs
+						if (!visited[r][c] && board[r][c] > day) {
+//							System.out.println(r+","+c);
+							bfs(day, r, c);
 							cnt++;
-							Queue<int[]> q = new LinkedList();
-							q.add(new int[] {i,j});
-							visited[i][j] = true;
-							
-							while(!q.isEmpty()) {
-								int[] place = q.poll();
-								int r = place[0];
-								int c = place[1];
-								
-								for (int d = 0; d < 4; d++) {
-									int nr = r + dr[d];
-									int nc = c + dc[d];
-									if(nr>=0 && nr<N && nc>=0 && nc<N && !visited[nr][nc] && cheese[nr][nc]>day) {
-										q.add(new int[] {nr,nc});
-										visited[nr][nc] = true;
-									}
-								}
-							}
-							
 						}
-					} // forj
-				} // fori
+					}
+				}
+
 				max = Math.max(max, cnt);
-			} // for day
-			
-			
-			
-			System.out.println("#"+tc+" "+max);
+			}
+
+			System.out.println("#" + tc + " " + max);
 		}
 
+	}
+
+	static void bfs(int day, int x, int y) {
+		visited[x][y] = true;
+
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] { x, y });
+
+		while (!q.isEmpty()) {
+			int[] place = q.poll();
+			int r = place[0];
+			int c = place[1];
+
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+
+				if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc] && board[nr][nc] > day) {
+					visited[nr][nc] = true;
+					q.add(new int[] { nr, nc });
+				}
+			}
+		}
 	}
 
 }
