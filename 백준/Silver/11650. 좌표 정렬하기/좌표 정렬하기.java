1b3ff5ffec0fbab0c7c55ff474bc96cb68f9c2eb
young5306 <140944183+young5306@users.noreply.github.com>
2025-06-04 15:26:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 632 ms, Memory: 60216 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,50 +1,50 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static class Node{
+	static class Node implements Comparable<Node> {
 		int x;
 		int y;
 		
-		Node (int x, int y){
+		public Node() {};
+		public Node(int x, int y) {
 			this.x = x;
 			this.y = y;
+		}
+		
+		@Override
+		public int compareTo(Node o){
+			if(this.x == o.x) {
+				return Integer.compare(this.y, o.y);
+			}
+			return this.x - o.x;
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		List<Node> list = new LinkedList<>();
 		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
+		int N = Integer.parseInt(br.readLine());
+		
+		Node[] arr = new Node[N];
+		for (int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			list.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-		}
-		
-		Collections.sort(list, new Comparator<Node>() {
-
-			@Override
-			public int compare(Node o1, Node o2) {
-				if(o1.x != o2.x) {
-					return o1.x - o2.x;
-				} else {
-					return o1.y - o2.y;
-				}
-			}
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 			
-		});
-		
-		for(Node n : list) {
-			System.out.println(n.x+" "+n.y);
+			arr[i] = new Node(x, y);
 		}
-
+		
+		Arrays.sort(arr);
+		
+		StringBuilder sb = new StringBuilder();
+		for(Node n : arr) {
+			sb.append(n.x).append(" ").append(n.y).append("\n");
+		}
+		System.out.println(sb);
 	}
-
 }
