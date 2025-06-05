79991d48fc98cabf2b7af23d087e8fdf1fb6dc18
young5306 <140944183+young5306@users.noreply.github.com>
2025-06-05 12:29:32 +0900

    [D3] Title: 햄버거 다이어트, Time: 153 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,65 +1,63 @@
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
+	
+	static int N, L, max;
+	static Ingredient[] ing;
+	
+	static class Ingredient {
+		int t;
+		int c;
+		
+		public Ingredient() {}
+		public Ingredient(int t, int c) {
+			this.t = t;
+			this.c = c;
+		}
+	}
 
-    // 주어진 제한 칼로리 이하의 조합 중 가장 맛점수가 높은 햄버거의 점수
-    // 부분집합
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		for(int tc=1; tc<=T; tc++) {
+			
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			
+			ing = new Ingredient[N];
+			for(int i=0; i<N; i++) {
+				st = new StringTokenizer(br.readLine());
+				int t = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				ing[i] = new Ingredient(t, c);
+			}
+			
+			// dfs
+			max = 0; // 최대점수
+			dfs(0, 0, 0); // 인덱스(중복x), 맛점수합, 칼로리합 
+			
+			System.out.println("#"+tc+" "+max);
+		}
+	}
+	
+	public static void dfs(int idx, int scoreSum, int calSum) {
+		// 기저 조건
+		if(calSum > L) return;
+		if(idx == N) {
+			if(calSum <= L) max = Math.max(max, scoreSum);
+			return;
+		}
+		
+		// 선택한 경우
+		dfs(idx+1, scoreSum + ing[idx].t, calSum + ing[idx].c);
+		// 선택x
+		dfs(idx+1, scoreSum, calSum);
+	}
 
-    static boolean[] sel;
-    static int[] taste;
-    static int[] calory;
-    static int N, L, tasteSum, calorySum, tasteMax;
-
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-
-        for (int tc = 1; tc <= T; tc++) {
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken()); // 재료 수
-            L = Integer.parseInt(st.nextToken()); // 제한 칼로리
-
-            taste = new int[N];
-            calory = new int[N];
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                taste[i] = Integer.parseInt(st.nextToken());
-                calory[i] = Integer.parseInt(st.nextToken());
-            }
-
-//            System.out.println(Arrays.toString(taste));
-//            System.out.println(Arrays.toString(calory));
-
-            // 부분집합 (재귀)
-            tasteMax = 0;
-            perm(0, 0, 0); // idx, tasteSum, calorySum
-
-            System.out.println("#"+tc+" "+tasteMax);
-        } // for tc
-    } // for main
-
-    public static void perm(int idx, int tasteSum, int calorySum){
-        // 기저 조건
-        if(calorySum > L) {
-            tasteMax = Math.max(tasteMax, tasteSum-taste[idx-1]);
-            return;
-        }
-        if(idx == N) {
-            tasteMax = Math.max(tasteMax, tasteSum);
-            return;
-        }
-
-//        System.out.println(idx+", "+tasteSum+", "+calorySum);
-        
-        // 사용
-        perm(idx+1, tasteSum+taste[idx], calorySum+calory[idx]);
-        // 사용 x
-        perm(idx+1, tasteSum, calorySum);
-    }
 }
