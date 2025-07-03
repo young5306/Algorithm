eb550f0df8d53b2da2c811f7a56df067bd1c0712
young5306 <140944183+young5306@users.noreply.github.com>
2025-07-03 15:47:40 +0900

    [D3] Title: 0/1 Knapsack, Time: 95 ms, Memory: 27,472 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -5,32 +6,29 @@ import java.util.StringTokenizer;
 public class Solution {
 
 	public static void main(String[] args) throws Exception {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		
-		for (int tc = 1; tc <= T; tc++) {
+		for(int tc=1; tc<=T; tc++) {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int K = Integer.parseInt(st.nextToken());
-			
+						
 			int[] volume = new int[N];
 			int[] cost = new int[N];
-			for (int i = 0; i < N; i++) {
+			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
 				volume[i] = Integer.parseInt(st.nextToken());
 				cost[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			// 1. 0~K부피까지 고려
-			// 2. 정보 저장
-			// 3. 메모테이블(dp) 만들기
-			int[] dp = new int[K+1]; // 0~K
-			// 4. 비교
-			for (int i = 0; i < N; i++) {
-				for (int k = K; k >= volume[i]; k--) {
-					dp[k] = Math.max(dp[k], dp[k-volume[i]]+cost[i]);
+			int[] dp = new int[K+1]; // 각 부피에서 최대가치 저장 및 갱신
+			// 1번~N번 물건들을 차근차근 확인
+			// 각 물건을 누적해가며 K~0부피(역순) 차근차근 확인
+			for(int i=0; i<N; i++) {
+				for(int k=K; k >= volume[i]; k--) {
+					dp[k] = Math.max(dp[k], dp[k-volume[i]] + cost[i]);
 				}
 			}
 			
