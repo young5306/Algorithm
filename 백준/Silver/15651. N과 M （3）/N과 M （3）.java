bd1aa33a58221e3f1aa2db1e1f0c1f97c4cb642f
young5306 <140944183+young5306@users.noreply.github.com>
2025-07-14 15:39:45 +0900

    [Silver III] Title: N과 M (3), Time: 356 ms, Memory: 112012 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,41 +1,45 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
+
+	static int[] num;
+	static int N, M;
+	static StringBuilder sb;
 	
 	public static void main(String[] args) throws Exception {
-		// 1~N 중 M개 고르기 (중복O, 순서O) -> 순열 (visited 빼면됨)
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		int[] sel = new int[M];
-		perm(N, M, sel, 0);
+		num = new int[M];
+		dfs(0);
 		
 		System.out.println(sb);
 	}
 	
-	public static void perm(int N, int M, int[] sel, int sidx) {
+	static void dfs(int sidx) {
 		// 기저 조건
-		if(sidx==M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(sel[i]).append(" ");
+		if(sidx >= M) {
+			for(int n: num) {
+				sb.append(n).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
 		
 		// 재귀 부분
-		for (int i = 1; i <= N; i++) {
-			// 선택O
-			sel[sidx] = i;
-			perm(N, M, sel, sidx+1);
+		for(int i=1; i<=N; i++) {
+			// 선택o
+			num[sidx] = i;
+			dfs(sidx+1);
+			// 선택x
 		}
 	}
+	
+
 }
