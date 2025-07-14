b43a82368e51c693ee0099b5e70c0b4e3820a7e1
young5306 <140944183+young5306@users.noreply.github.com>
2025-07-14 18:15:52 +0900

    [Silver III] Title: N과 M (8), Time: 96 ms, Memory: 19876 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,51 +1,49 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static StringBuilder sb = new StringBuilder();
 
+	static int N, M;
+	static int[] num;
+	static int[] sel;
+	static StringBuilder sb;
+	
 	public static void main(String[] args) throws Exception {
-		
-		// N개 중 M개 선택 (중복O, 순서X) -> 중복O 조합 
-		// 사전 순 출력 -> nums 정렬
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		int[] nums = new int[N];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
+		num = new int[N];
+		for(int i=0; i<N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(nums);
-		int[] sel = new int[M];
-		combination(N, M, nums, sel, 0, 0);
+		Arrays.sort(num);
 		
+		sel = new int[M];
+		dfs(0, 0);
+
 		System.out.println(sb);
 	}
 	
-	public static void combination(int N, int M, int[] nums, int[] sel, int idx, int sidx) {
+	static void dfs(int idx, int sidx) {
 		// 기저 조건
-		if(sidx==M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(sel[i]).append(" ");
+		if(sidx >= M) {
+			for(int s: sel) {
+				sb.append(s).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
 		
 		// 재귀 부분
-		for (int i = idx; i < N; i++) {
-			// 선택O
-			sel[sidx] = nums[i];
-			combination(N, M, nums, sel, i, sidx+1);
+		for(int i=idx; i<N; i++) {
+			sel[sidx] = num[i];
+			dfs(i, sidx+1);
 		}
 	}
-
-}
+}  
