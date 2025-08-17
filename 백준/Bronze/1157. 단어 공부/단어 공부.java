c611f1df584563b0a4d64661ad602296de541584
young5306 <140944183+young5306@users.noreply.github.com>
2025-08-17 17:31:16 +0900

    [Bronze I] Title: 단어 공부, Time: 152 ms, Memory: 20580 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Map;
@@ -7,32 +8,30 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		
-		Map<Character, Integer> map = new TreeMap<>();
-		for(int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-			if(ch >= 'A' && ch <= 'Z') ch = (char) (ch - 'A' + 'a'); // (a - A = b - B)
-			map.put(ch, map.getOrDefault(ch, 0) + 1);
-		}
-		
-		int max = 0;
-		for(int v : map.values()) {
-			max = Math.max(max, v);
-		}
-		
-		int cnt = 0;
-		char key = ' ';
-		for(Map.Entry<Character, Integer> e : map.entrySet()) {
-//			System.out.println(e.getKey());
-			if(e.getValue() == max) {
-				cnt++;
-				key = e.getKey();
-			}
-			
-		}
-		
-		System.out.println(cnt == 1 ? (char) (key - 'a' + 'A') : '?');
+        String s = br.readLine();
+
+        int[] cnt = new int[26]; // A~Z 빈도
+        for (int i = 0, n = s.length(); i < n; i++) {
+            char c = s.charAt(i);
+            // 소문자면 대문자로 변화
+            if ('a' <= c && c <= 'z') c = (char)(c - 'a' + 'A');
+            cnt[c - 'A']++;
+        }
+
+        int max = -1, idx = -1;
+        boolean tie = false;
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] > max) {
+                max = cnt[i];
+                idx = i;
+                tie = false; // 새로운 최댓값 발견 -> 동률 해제
+            } else if (cnt[i] == max) {
+                tie = true; // 최댓값과 동률
+            }
+        }
+
+        if (tie) System.out.println("?");
+        else System.out.println((char) ('A' + idx));
 	}
 
 }
