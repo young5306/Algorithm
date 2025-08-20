28e388fd91301b90914e5296af5f2511ab8a1617
young5306 <140944183+young5306@users.noreply.github.com>
2025-08-21 01:03:36 +0900

    [Silver III] Title: N과 M (1), Time: 1852 ms, Memory: 86080 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,43 +3,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int[] num;
+	static boolean[] visited;
+	static int N, M;
 
 	public static void main(String[] args) throws Exception {
-		// 1~N 중 중복없이 M개 고른 수열 모두 구해서 출력
-		// 중복 X 순열
+		
+		// 중복없이 N중M 고르기 (순서x)
+		// dfs
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		int[] sel = new int[M];
-		boolean[] visited = new boolean[N+1];
-		perm(N, M, sel, visited, 0);
-
+		num = new int[M];
+		visited = new boolean[N+1];
+		dfs(0);
+		
 	}
 	
-	// 순열은 sidx만 필요 (조합은 idx도 필요)
-	public static void perm(int N, int M, int[] sel, boolean[] visited, int sidx) {
+	static void dfs(int sidx) {
 		// 기저 조건
-		if(sidx==M) {
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < M; i++) {
-				sb.append(sel[i]).append(" ");
+		if(sidx >= M) {
+			for(int n: num) {
+				System.out.print(n+" ");
 			}
-			System.out.println(sb);
+			System.out.println();
 			return;
 		}
 		
 		// 재귀 부분
-		for (int i = 1; i <= N; i++) { // 순서 있음
-			// 선택 O
-			if(!visited[i]) {
-				sel[sidx] = i;
-				visited[i] = true;
-				perm(N, M, sel, visited, sidx+1);
-				visited[i]=false; // 원상복구
-			}
+		for(int i=1; i<=N; i++) {
+			// 선택o
+			if(visited[i]) continue;
+			visited[i] = true;
+			num[sidx] = i;
+			dfs(sidx+1);
+			// 선택x
+			visited[i] = false;
 		}
 	}
+
 }
