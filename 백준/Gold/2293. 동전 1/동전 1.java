58436291139db9e4096c7f63c493df6dd6ee5d90
young5306 <140944183+young5306@users.noreply.github.com>
2025-09-29 13:47:40 +0900

    [Gold IV] Title: 동전 1, Time: 76 ms, Memory: 11944 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static final int INF = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		
+		int[] dp = new int[K + 1];
+		dp[0] = 1;
+		
+		for(int n = 0; n < N; n++) {
+			for(int k = arr[n]; k <= K; k++) {
+				if(dp[k - arr[n]] != 0) {
+					dp[k] += dp[k - arr[n]];					
+				}
+			}
+//			System.out.println(Arrays.toString(dp));
+		}
+		
+		System.out.println(dp[K]);
+	}
+
+}
