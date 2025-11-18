38b89354dee02e8cd45d866bbb58254cdfa08587
young5306 <140944183+young5306@users.noreply.github.com>
2025-11-18 23:06:01 +0900

    [level 2] Title: 요격 시스템, Time: 366.72 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -3,26 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         
-        // 시작점으로 정렬
-        // 끝점
+        // 끝점으로 정렬
+        // 끝점 - 0.1에서 요격한다고 생각
+        // 끝점이 다음 시작점들보다 작아질 때, 요격 횟수 + 1
         
         Arrays.sort(targets, new Comparator<int[]>(){
             @Override
             public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
+                return a[1] - b[1];
             }
         });
         // Arrays.sort(target, (a, b) -> (a[0] - b[0]));
         
         int answer = 1;
-        int prevEndMin = targets[0][1];
-        for(int i = 0; i < targets.length; i++) {
-            if(prevEndMin <= targets[i][0]) {
-                answer++; // 요격 횟수 +1 증가
-                prevEndMin = targets[i][1]; // 끝점 최솟값 초기화
-            } else {
-                prevEndMin = Math.min(prevEndMin, targets[i][1]);
-            }  
+        int e = targets[0][1];
+        for(int i = 1; i < targets.length; i++) {
+             if(e <= targets[i][0]) {
+                 answer++;
+                 e = targets[i][1];
+             }
         }
         
         return answer;
